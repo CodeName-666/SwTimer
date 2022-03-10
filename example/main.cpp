fd03b6bfe4661572c9914ab76d0eefba07ba3026
SeidC <SeidelChristof@googlemail.com>
2022-03-10 16:04:58 +0100

    Remove old code

--- example/main.cpp
+++ example/main.cpp
@@ -1,20 +1,9 @@
 #include <Arduino.h>
-#include "FiFo.h"
+
 
 
 void setup() {
-    FiFo<uint16_t> fifo_one;
-    uint16_t a = 5, b = 10, c = 20;
-    Serial.begin(9600);
-    
-    fifo_one.write(&a);
-    fifo_one.write(&b);
-    fifo_one.write(&c);
-
-    Serial.print("Last value = "); Serial.println(fifo_one.read());
-    Serial.print("Next value = "); Serial.println(fifo_one.read());
-    Serial.print("Next value = "); Serial.println(fifo_one.read());
-    Serial.print("Next value = "); Serial.println(fifo_one.read());
+   /*TBD*/
 }
 
 void loop() {
